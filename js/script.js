c8e4300c37218e43d522ddb3d2e9c65d6498c1c2
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2022-10-22 12:25:57 +0200

    Add js script playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Random Computer Move
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -17,3 +18,22 @@ if (randomNumber == '3') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+//Player Move
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+  printMessage('Twój ruch: ' + playerMove);
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+  printMessage('Twój ruch' + playerMove);
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+  printMessage('Twój ruch' + playerMove);
+} else {
+  playerMove = 'nieznany ruch';
+  printMessage('nieznany ruch');
+}
